f9efc857e7b3face95486acdcd395dd46662684f
Alejandro <jesusrods312@gmail.com>
2021-11-03 08:15:07 +0100

    Exercises day 1

---
+++ day_1/helloworld.py
@@ -0,0 +1,31 @@
+# Exercise: Level 2
+
+import sys
+
+print('\n1. Version Python')
+print('Version:', sys.version)
+
+print('\n2. Operations')
+print('addition:', 3+4)
+print('subtraction:', 3-4)
+print('multiplication:', 3*4)
+print('modulus:', 3%4)
+print('division:', 3/4)
+print('exponential:', 3**4)
+print('floor division operator:', 3//4)
+
+print('\n3. Strings')
+print('Your name: Alejandro')
+print('Your family name: Rodríguez')
+print('Your country: Argentina')
+print('I am enjoying 30 days of python')
+
+print('\n4. Types')
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('Alejandro'))
+print(type('Rodríguez'))
+print(type('Argentina'))
---
+++ day_1/level_3.py
@@ -0,0 +1,15 @@
+# Exercise: Level 3
+
+print('\n1. Write an example for different Python data types such as Number(Integer, Float, Complex), String, Boolean, List, Tuple, Set and Dictionary.')
+print('Integer:', 5)
+print('Float:', 5.3)
+print('Complex:', 5 - 3j)
+print('String: This is an string')
+print('Boolean:', True)
+print('List:', [1, 2, 3, 4, 5])
+print('Tuple:', (1, 2, 3, 4, 5))
+print('Set:', {1, 2, 3, 4, 5})
+print('Dictionary:', {'a':1, 'b':2, 'c':3, 'd':4, 'e':5})
+
+print('\n2. Find an Euclidian distance between (2, 3) and (10, 8)')
+print('The Euclidian distance between the two points is:', ((10 - 2)**2 + (8 - 3)**2)**(1/2))
