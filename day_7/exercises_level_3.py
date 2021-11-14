ee97c98a4bfcb8f1aec171cf6d27e2783e487e49
Alejandro <jesusrods312@gmail.com>
2021-11-14 13:17:14 +0100

    Exercises day 7

--- day_7/exercises_level_3.py
+++ day_7/exercises_level_3.py
@@ -1,3 +1,24 @@
 # Day 7: Exercises level 3
 
+print()
 age = [22, 19, 24, 25, 26, 24, 25, 24]
+print(age)
+print()
+
+# 1
+
+age_set = set(age)
+length_lt = len(age)
+length_st = len(age_set)
+print(age_set)
+print()
+print('The length of the list is', length_lt, 'while the length of the set is', length_st)
+print()
+
+# 3
+
+sentence = 'I am a teacher and I love to inspire and teach people'
+print(sentence)
+print()
+print('This sentence has', len(set(sentence.split(' '))), 'unique words.')
+
